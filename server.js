6c9c84096febfa41fe0268114b08462aa793c4e2
José Ulombe <jose.ulombe@hotmail.com>
2020-09-21 17:56:13 +0100

    configuring nunjucks

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const nunjucks = require('nunjucks')
 const server = express()
 
 
+//configure template engine
+server.set("view engine", "html")
+nunjucks.configure("views", {
+    express: server
+})
+
+
+
 //start server
 server.listen(3000, function() {
     console.log("server is running")
